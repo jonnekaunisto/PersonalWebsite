51a75ec0ebf33eafe04cbb2991158f70a49aea18
Jonne Kaunisto <kaunisto@Jonnes-MacBook-Air-2.local>
2025-05-06 23:34:24 -0600

    Refactored main page to react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,46 @@
 import Image from "next/image";
+import { FaGithub, FaLinkedin, FaTrophy } from "react-icons/fa";
+import { IoIosInformationCircleOutline } from "react-icons/io";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	return (
+		<div style={{
+			background: 'linear-gradient(44deg,rgba(137, 98, 163, 1) 0%, rgba(242, 92, 92, 1) 50%, rgba(242, 182, 97, 1) 100%);',
+			backgroundSize: 'cover',
+			backgroundRepeat: 'no-repeat',
+			height: '100vh'
+		}}>
+			<div id="wrapper" className="h-screen flex items-center justify-center text-black">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+				<section className="bg-white p-5 m-50 rounded-lg">
+					<header>
+						<span className="avatar flex items-center gap-4">
+							<hr className="h-px w-30 bg-gray-300 border-0"></hr>
+							<Image src="/Jonne.jpeg" width="150" height="150" alt="" className="rounded-full" />
+							<hr className="h-px w-30 bg-gray-300 border-0"></hr>
+						</span>
+						<h1 className="text-5xl text-center font-alumni">Jonne Kaunisto</h1>
+						<p className="text-2xl text-center font-alumni">Software Engineer</p>
+					</header>
+
+					<footer>
+						<ul className="icons flex justify-center gap-4 mt-4">
+							<a href="https://github.com/jonnekaunisto">
+								<FaGithub className="size-7 hover:fill-red-300" />
+							</a>
+							<a href="https://www.linkedin.com/in/jonnekaunisto/">
+								<FaLinkedin className="size-7 hover:fill-blue-300" />
+							</a>
+							<a href="achievements.html">
+								<FaTrophy className="size-7 hover:fill-green-300" />
+							</a>
+							<a href="projects.html">
+								<IoIosInformationCircleOutline className="size-7 hover:fill-purple-300" />
+							</a>
+						</ul>
+					</footer>
+				</section>
+			</div>
+		</div>
+	);
 }
