8866991d31ccfdfb9a4009ae1c178178c23337cb
Jonne Kaunisto <kaunisto@Jonnes-MacBook-Air-2.local>
2025-06-20 18:45:38 -0600

    Fixed fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Alumni_Sans_Pinstripe } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +10,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const alumniSans = Alumni_Sans_Pinstripe({
+  variable: "--font-alumni-sans-pinstripe",
+  subsets: ["latin"],
+  weight: "400"
 });
 
 export const metadata: Metadata = {
@@ -26,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${alumniSans.variable} antialiased`}
       >
         {children}
       </body>
